9871b69725cd3828b8a8c428621702845661aa53
ALRUTEN <laurentnavarro31@gmail.com>
2017-10-23 11:25:32 +0200

    regex

--- app/src/main/java/com/example/bastienwcs/atelierregex/MainActivity.java
+++ app/src/main/java/com/example/bastienwcs/atelierregex/MainActivity.java
@@ -7,6 +7,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,39 +25,62 @@ public class MainActivity extends AppCompatActivity {
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 boolean isNameOk = false;
-                // TODO: check Name here
+                String nameContent = name.getText().toString();
+                Pattern pattern = Pattern.compile("^[\\-a-zA-Z ]+$");
+                Matcher m = pattern.matcher(nameContent);
+                while (m.find()) {
+                    isNameOk = true;
+                }
 
                 if (!isNameOk) {
                     name.setError(getResources().getString(R.string.error_name));
                 }
 
                 boolean isAgeOk = false;
-                // TODO: check Age here
+                String ageContent = age.getText().toString();
+                Pattern pattern1 = Pattern.compile("^[0-9]{3}+$");
+                Matcher m1 = pattern1.matcher(ageContent);
+                while (m1.find()) {
+                    isNameOk = true;
+                }
 
                 if (!isAgeOk) {
                     age.setError(getResources().getString(R.string.error_age));
                 }
 
                 boolean isAddressOk = false;
-                // TODO: check Address here
+                String adresseContent = age.getText().toString();
+                Pattern pattern2 = Pattern.compile("[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)*");
+                Matcher m2 = pattern2.matcher(adresseContent);
+                while (m2.find()) {
+                    isNameOk = true;
+                }
 
                 if (!isAddressOk) {
                     address.setError(getResources().getString(R.string.error_address));
                 }
 
                 boolean isEmailOk = false;
-                // TODO: check Email here
+                String emailContent = age.getText().toString();
+                Pattern pattern3 = Pattern.compile("^[\\w.-]+@[\\w.-]{2,}\\.[a-z]{2,5}$\n");
+                Matcher m3 = pattern3.matcher(emailContent);
+                while (m3.find()) {
+                    isNameOk = true;
+                    if (!isEmailOk) {
+                        email.setError(getResources().getString(R.string.error_email));
+                    }
 
-                if (!isEmailOk) {
-                    email.setError(getResources().getString(R.string.error_email));
-                }
-
-                if (isNameOk && isAgeOk && isAddressOk && isEmailOk) {
-                    Toast.makeText(MainActivity.this,
-                            getResources().getString(R.string.success), Toast.LENGTH_LONG).show();
+                    if (isNameOk && isAgeOk && isAddressOk && isEmailOk) {
+                        Toast.makeText(MainActivity.this,
+                                getResources().getString(R.string.success), Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         });
+
     }
 }
+
+
